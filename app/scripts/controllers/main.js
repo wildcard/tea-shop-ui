f7b0d58a6aa92e1418fb11d3da9926f4ab1ca316
Yakov Kadosh <kobi.kadosh@gmail.com>
2016-02-02 00:58:18 +0200

    angular ui boilerplate

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name teaShopUiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the teaShopUiApp
+ */
+angular.module('teaShopUiApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
